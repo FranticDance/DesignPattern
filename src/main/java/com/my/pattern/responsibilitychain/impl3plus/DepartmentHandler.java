4d28d076a2c327bc8adfef6c159f78001751b013
lpy <forwardlpy@aliyun.com>
2019-12-20 14:16:25 +0800

    新增责任链模式的其中一个实现方式的改进版本

---
+++ src/main/java/com/my/pattern/responsibilitychain/impl3plus/DepartmentHandler.java
@@ -0,0 +1,25 @@
+package com.my.pattern.responsibilitychain.impl3plus;
+
+/**
+ * @author lee
+ * 系处理器
+ */
+public class DepartmentHandler implements Handler {
+    @Override
+    public void handle(PurchaseRequest purchaseRequest, HandlerChain handlerChain) {
+        //TODO 真正的业务逻辑
+        System.out.println("系主任DepartmentHandler开始处理");
+        if(purchaseRequest.getPrice() <= 1000){
+            System.out.println("系主任DepartmentHandler will deal it");
+
+            //将请求交给下一位处理器处理，这样的效果就是，整个链条，不管链条上的handler能不能处理，整个链条都会遍历一遍
+            //如果说，需要达到，当某一个handler能够处理，就停止遍历，不再交由下游handler处理，就将isStopTraversal设置为true。
+            handlerChain.process(purchaseRequest, true);
+        }else {
+            System.out.println("系主任无法处理，交给下一位");
+            handlerChain.process(purchaseRequest, false);
+        }
+
+
+    }
+}
