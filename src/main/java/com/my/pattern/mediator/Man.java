80e816d5d1a242d172161835dd87a0e5d4ab7ce5
lpy <forwardlpy@aliyun.com>
2020-11-09 17:16:17 +0800

    增加中介者模式代码

---
+++ src/main/java/com/my/pattern/mediator/Man.java
@@ -0,0 +1,19 @@
+package com.my.pattern.mediator;
+
+/**
+ * 具体同事类
+ * @author lee
+ * @version 1.0
+ * @date 2020/11/9 15:35
+ */
+public class Man extends Person{
+    public Man(String name, int condition, Mediator mediator) {
+        super(name, condition, mediator);
+    }
+
+    @Override
+    public void findCompanion(Person person) {
+        this.getMediator().setMan(this);
+        this.getMediator().introduce(person);
+    }
+}
---
+++ src/main/java/com/my/pattern/mediator/YuelaoMediator.java
@@ -0,0 +1,35 @@
+package com.my.pattern.mediator;
+
+/**
+ * 具体中介者
+ * @author lee
+ * @version 1.0
+ * @date 2020/11/9 15:33
+ */
+public class YuelaoMediator extends Mediator{
+    /**
+     * 具体的业务逻辑
+     * @param person
+     */
+    @Override
+    public void introduce(Person person) {
+        if(person instanceof Woman){
+            this.setWoman(person);
+        }
+        if(person instanceof Man){
+            this.setMan(person);
+        }
+        if(this.getMan() == null ||  this.getWoman() == null){
+            System.out.println("oh no");
+            return;
+        }
+        if (this.getWoman().getCondition() == this.getMan().getCondition()) {
+            System.out.println(this.getMan().getName() + "和" + this.getWoman().getName() + "很合适");
+            return;
+        }else {
+            System.out.println(this.getMan().getName() + "和" + this.getWoman().getName() + "不合适");
+            return;
+        }
+
+    }
+}
